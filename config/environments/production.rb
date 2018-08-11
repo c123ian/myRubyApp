ec3d22eb72cfe06d504ff74e8ea0dfb5e79566c9
Cian Prendergast <c123ian@gmail.com>
2018-08-11 14:22:34 +0100

    trying to fix images heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Heroku background images fix
+  config.assets.compile = true
+  config.serve_static_files = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
